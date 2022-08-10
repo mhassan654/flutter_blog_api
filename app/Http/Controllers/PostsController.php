641b98fae82249c9e20a381e123b7b6883a1ff17
Muwonge Hassan <hassansaava@gmail.com>
2022-08-10 19:11:29 +0300

    resolve post update issue

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,22 +10,22 @@ class PostsController extends Controller
     public function index()
     {
         return response([
-            'posts' =>Post::orderBy('created_at','desc')
-            ->with('user:id,name,image')
-            ->withCount('comments','likes')
-            ->with('likes', function($like) {
-                return $like->where('user_id',auth()->user()->id)->select('id','user_id','post_id')->get();
-            })
-            ->get()
-        ],200);
+            'posts' => Post::orderBy('created_at', 'desc')
+                ->with('user:id,name,image')
+                ->withCount('comments', 'likes')
+                ->with('likes', function ($like) {
+                    return $like->where('user_id', auth()->user()->id)->select('id', 'user_id', 'post_id')->get();
+                })
+                ->get(),
+        ], 200);
     }
 
     // get single post from
     public function show($id)
     {
         return response([
-            'post'=>Post::where('id', $id)->withCount('comments','likes')->get()
-        ],200);
+            'post' => Post::where('id', $id)->withCount('comments', 'likes')->get(),
+        ], 200);
     }
 
     public function store(Request $request)
@@ -39,51 +39,50 @@ class PostsController extends Controller
         // }
 
         $attrs = $request->validate([
-            'body' => 'required|string'
+            'body' => 'required|string',
         ]);
 
         $image = $this->saveImage($request->image, 'posts');
 
         $post = Post::create([
             'body' => $attrs['body'],
-            'user_id'=>auth()->user()->id,
-            'image' => $image
+            'user_id' => auth()->user()->id,
+            'image' => $image,
         ]);
 
         return response([
-            'message' =>'Post created successfully.',
-            'post'=>$post
-        ],200);
+            'message' => 'Post created successfully.',
+            'post' => $post,
+        ], 200);
     }
 
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
         $post = Post::find($id);
 
         if (!$post) {
 
-           return response([
-               'message' => 'post not found'
-           ],403);
+            return response([
+                'message' => 'post not found',
+            ], 403);
         }
         if ($post->user_id !== auth()->user()->id) {
             return response([
-                'message' => 'Permission Denied.'
-            ],403);
+                'message' => 'Permission Denied.',
+            ], 403);
         }
 
         $attrs = $request->validate([
-            'body' => 'required|string'
+            'body' => 'required|string',
         ]);
 
-        $post= Post::update([
-            'body' => $attrs['body']
-        ]);
+        $post->body = $attrs['body'];
+        $post->update();
 
         return response([
-            'message' =>'post updated.',
-            'post'=>$post
-        ],200);
+            'message' => 'post updated.',
+            'post' => $post,
+        ], 200);
     }
 
     public function destroy($id)
@@ -92,12 +91,12 @@ class PostsController extends Controller
 
         if (!$post) {
             return response([
-                'message'=>'Post not founc.'
-            ],403);
+                'message' => 'Post not founc.',
+            ], 403);
         }
 
         if ($post->user_id !== auth()->user()->id) {
-             return response(['message'=>'Permission denied.'],403);
+            return response(['message' => 'Permission denied.'], 403);
         }
 
         $post->comments()->delete();
@@ -105,7 +104,7 @@ class PostsController extends Controller
         $post->delete();
 
         return response([
-            'message' =>'post deleted.'
-        ],200);
+            'message' => 'post deleted.',
+        ], 200);
     }
 }
